657633c5c09184f17f23a56bb28b9ba28b271694
matee999 <matija.pleskonjic99@gmail.com>
2020-11-05 13:19:30 +0100

    Napisan Query za Department

--- src/main/java/student_administration/repositories/DepartmentRepository.java
+++ src/main/java/student_administration/repositories/DepartmentRepository.java
@@ -1,7 +1,16 @@
 package student_administration.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import student_administration.models.Department;
+import student_administration.models.Subject;
 
-public interface DepartmentRepository extends CrudRepository<Department, Integer> {}
+public interface DepartmentRepository extends CrudRepository<Department, Integer> {
+	
+	@Query("SELECT su FROM Subject su WHERE su.department = "
+			+ "(SELECT department.subjects FROM Department department WHERE department.departmentId = :departmentId)")
+	List<Subject> getSubjectByDepartmentId(int departmentId);
+}
--- src/test/java/repositories/DepartmentRepositoryTest.java
+++ src/test/java/repositories/DepartmentRepositoryTest.java
@@ -1,11 +1,30 @@
 package repositories;
 
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import student_administration.models.Subject;
+import student_administration.repositories.DepartmentRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DepartmentRepositoryTest {
 
+	@Autowired
+	DepartmentRepository departmentRepository;
+	
+	@Test
+	public void getSubjectOnDepartment() throws Exception{
+		System.out.println("---getSubjectOnDepartment---");
+		List<Subject> subjects = departmentRepository.getSubjectByDepartmentId(1);
+		
+		for(Subject s : subjects) {
+			System.out.println(s);
+		}
+	}
 }
